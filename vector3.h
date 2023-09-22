c2d4297ca4c6d44b792612106b2ce33688281de2
l1nky-1337 <145037011+l1nky-1337@users.noreply.github.com>
2023-09-22 07:22:09 +0300

    Add files via upload

---
+++ vector3.h
@@ -0,0 +1,45 @@
+class Vector3
+{
+public:
+	Vector3() : x(0.f), y(0.f), z(0.f)
+	{
+
+	}
+
+	Vector3(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+	{
+
+	}
+	~Vector3()
+	{
+
+	}
+
+	double x;
+	double y;
+	double z;
+
+	inline float Dot(Vector3 v)
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	inline float Distance(Vector3 v)
+	{
+		return float(sqrtf(powf(v.x - x, 2.0) + powf(v.y - y, 2.0) + powf(v.z - z, 2.0)));
+	}
+
+	Vector3 operator+(Vector3 v)
+	{
+		return Vector3(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vector3 operator-(Vector3 v)
+	{
+		return Vector3(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vector3 operator*(float number) const {
+		return Vector3(x * number, y * number, z * number);
+	}
+};
